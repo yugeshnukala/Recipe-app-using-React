319b857b5db64d6e93e10eb0018fc2c26a5f9e9b
yugeshnukala <yugeshnukala95@gmail.com>
2020-08-01 14:36:33 +0530

    Updates

---
+++ precache-manifest.ac85d045d8aeffc7ebc19225f64bc7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ce04ee6d52d82801eed87f03f1b387",
+    "url": "/Recipe-app-using-React/index.html"
+  },
+  {
+    "revision": "78a33065f466008ac1ae",
+    "url": "/Recipe-app-using-React/static/css/main.f66474cf.chunk.css"
+  },
+  {
+    "revision": "6d67894d5ab4d3145ad6",
+    "url": "/Recipe-app-using-React/static/js/2.20dedf64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-app-using-React/static/js/2.20dedf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a33065f466008ac1ae",
+    "url": "/Recipe-app-using-React/static/js/main.ea6599ee.chunk.js"
+  },
+  {
+    "revision": "28d1c7d632b190664b1c",
+    "url": "/Recipe-app-using-React/static/js/runtime-main.116aa849.js"
+  }
+]);
